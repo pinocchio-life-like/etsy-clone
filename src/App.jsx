57dc887ae26185a49bcbbc80153e41778c1cf744
pinocchio-life-like <icbr19fl@gmail.com>
2024-08-14 09:44:48 +0300

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,73 +2,7 @@ import Header from "./components/Header";
 import FilterBar from "./components/FilterBar";
 import ProductGrid from "./components/ProductGrid";
 import FeaturedProduct from "./components/FeaturedProduct";
-
-const products = [
-  {
-    id: 1,
-    image: "https://via.placeholder.com/300",
-    title: "Football Field Pencil Case",
-    price: 19.0,
-    rating: 5,
-    shopName: "efratul",
-  },
-  {
-    id: 2,
-    image: "https://via.placeholder.com/150",
-    title: "Soccer Ball Necklace",
-    price: 50.79,
-    rating: 4,
-    shopName: "DreamBeadsDovi",
-  },
-  {
-    id: 1,
-    image: "https://via.placeholder.com/300",
-    title: "Football Field Pencil Case",
-    price: 19.0,
-    rating: 5,
-    shopName: "efratul",
-  },
-  {
-    id: 2,
-    image: "https://via.placeholder.com/150",
-    title: "Soccer Ball Necklace",
-    price: 50.79,
-    rating: 4,
-    shopName: "DreamBeadsDovi",
-  },
-  {
-    id: 1,
-    image: "https://via.placeholder.com/300",
-    title: "Football Field Pencil Case",
-    price: 19.0,
-    rating: 5,
-    shopName: "efratul",
-  },
-  {
-    id: 2,
-    image: "https://via.placeholder.com/150",
-    title: "Soccer Ball Necklace",
-    price: 50.79,
-    rating: 4,
-    shopName: "DreamBeadsDovi",
-  },
-  {
-    id: 1,
-    image: "https://via.placeholder.com/300",
-    title: "Football Field Pencil Case",
-    price: 19.0,
-    rating: 5,
-    shopName: "efratul",
-  },
-  {
-    id: 2,
-    image: "https://via.placeholder.com/150",
-    title: "Soccer Ball Necklace",
-    price: 50.79,
-    rating: 4,
-    shopName: "DreamBeadsDovi",
-  },
-];
+import products from "./constants/products";
 
 const App = () => {
   return (
